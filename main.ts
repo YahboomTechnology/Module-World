65eff9ae5c34b843a326d170ab049a0fdcec194b
IIQAQII <40288683+IIQAQII@users.noreply.github.com>
2020-12-23 16:25:30 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -182,17 +182,21 @@ namespace ModuleWorld_Sensor {
 			case 0:
 				let dhtvalue1 = 0;
 				let dhtcounter1 = 0;
+				let dhtcounter1d = 0;
 				while (pins.digitalReadPin(dht11pin) == 1);
 				while (pins.digitalReadPin(dht11pin) == 0);
 				while (pins.digitalReadPin(dht11pin) == 1);
 				for (let i = 0; i <= 32 - 1; i++) {
-					while (pins.digitalReadPin(dht11pin) == 0);
+					dhtcounter1d = 0
+					while (pins.digitalReadPin(dht11pin) == 0) {
+						dhtcounter1d += 1;
+					}
 					dhtcounter1 = 0
 					while (pins.digitalReadPin(dht11pin) == 1) {
 						dhtcounter1 += 1;
 					}
 					if (i > 15) {
-						if (dhtcounter1 > 2) {
+						if (dhtcounter1 > dhtcounter1d) {
 							dhtvalue1 = dhtvalue1 + (1 << (31 - i));
 						}
 					}
@@ -205,14 +209,18 @@ namespace ModuleWorld_Sensor {
 				while (pins.digitalReadPin(dht11pin) == 1);
 				let dhtvalue = 0;
 				let dhtcounter = 0;
+				let dhtcounterd = 0;
 				for (let i = 0; i <= 32 - 1; i++) {
-					while (pins.digitalReadPin(dht11pin) == 0);
+					dhtcounterd = 0
+					while (pins.digitalReadPin(dht11pin) == 0) {
+						dhtcounterd += 1;
+					}
 					dhtcounter = 0
 					while (pins.digitalReadPin(dht11pin) == 1) {
 						dhtcounter += 1;
 					}
 					if (i > 15) {
-						if (dhtcounter > 2) {
+						if (dhtcounter > dhtcounterd) {
 							dhtvalue = dhtvalue + (1 << (31 - i));
 						}
 					}
@@ -226,14 +234,18 @@ namespace ModuleWorld_Sensor {
 
 				let value = 0;
 				let counter = 0;
+				let counterd = 0;
 
 				for (let i = 0; i <= 8 - 1; i++) {
-					while (pins.digitalReadPin(dht11pin) == 0);
+					counterd = 0
+					while (pins.digitalReadPin(dht11pin) == 0) {
+						counterd += 1;
+					}
 					counter = 0
 					while (pins.digitalReadPin(dht11pin) == 1) {
 						counter += 1;
 					}
-					if (counter > 3) {
+					if (counter > counterd) {
 						value = value + (1 << (7 - i));
 					}
 				}
